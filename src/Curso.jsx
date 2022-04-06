d98089e388fca44bba30cd651a78f7b6110a6139
david-romero-shopperfact <david.romero@shopperfact.com>
2022-04-05 20:12:14 -0500

    modificando archivos y agregando curso.jsx y styles

---
+++ src/Curso.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+const Curso = () =>(
+   <article className="card">
+      <div className="card__layout">
+         <div className="img-container">
+            <img src="https://placeimg.com/200/100/tech" alt="" />
+         </div>
+         <div>
+            <h3>Curso PHP</h3>
+            <div><span>Victor Romero</span></div>
+            <div><a href="hola.com">Ir al curso</a></div>
+         </div>
+      </div>
+      <div className="card__layout">
+         <div className="img-container">
+            <img src="https://placeimg.com/200/100/tech" alt="" />
+         </div>
+         <div>
+            <h3>Curso PHP</h3>
+            <div><span>Victor Romero</span></div>
+            <div><a href="hola.com">Ir al curso</a></div>
+         </div>
+      </div>
+      <div className="card__layout">
+         <div className="img-container">
+            <img src="https://placeimg.com/200/100/tech" alt="" />
+         </div>
+         <div>
+            <h3>Curso PHP</h3>
+            <div><span>Victor Romero</span></div>
+            <div><a href="hola.com">Ir al curso</a></div>
+         </div>
+      </div>
+      <div className="card__layout">
+         <div className="img-container">
+            <img src="https://placeimg.com/200/100/tech" alt="" />
+         </div>
+         <div>
+            <h3>Curso PHP</h3>
+            <div><span>Victor Romero</span></div>
+            <div><a href="hola.com">Ir al curso</a></div>
+         </div>
+      </div>
+      <div className="card__layout">
+         <div className="img-container">
+            <img src="https://placeimg.com/200/100/tech" alt="" />
+         </div>
+         <div>
+            <h3>Curso PHP</h3>
+            <div><span>Victor Romero</span></div>
+            <div><a href="hola.com">Ir al curso</a></div>
+         </div>
+      </div>
+      <div className="card__layout">
+         <div className="img-container">
+            <img src="https://placeimg.com/200/100/tech" alt="" />
+         </div>
+         <div>
+            <h3>Curso PHP</h3>
+            <div><span>Victor Romero</span></div>
+            <div><a href="hola.com">Ir al curso</a></div>
+         </div>
+      </div>
+   </article>
+)
+
+export default Curso
