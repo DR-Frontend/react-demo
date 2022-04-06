8b45ca6ee498722a096c4b2c0c7f27ec804b5fca
david-romero-shopperfact <david.romero@shopperfact.com>
2022-04-06 03:40:40 -0500

    proptypes, defaultprops, mapeo de array para mostrar info en componente

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import Curso from './Curso';
 
+const cursos = [
+  {
+    'title': 'Curso web',
+    'profesor': 'Omar Romero',
+    'price': 10,
+    'image': 'https://placeimg.com/200/100/tech',
+    'alt': 'React JS'
+  },
+  {
+    'title': 'Curso css',
+    'profesor': 'Tzhacil Romero',
+    'price': 20,
+    'image': 'https://placeimg.com/200/100/tech',
+    'alt': 'React JS'
+  },
+  {
+    'title': 'Curso PHP',
+    'profesor': 'Victor Romero',
+    'price': 30,
+    'image': 'https://placeimg.com/200/100/tech',
+    'alt': 'React JS'
+  }
+]
+
 const App = () => (
   <>
     <div>
-      <h3>Hola mundo desde React JS</h3>
-    </div>
-    <div>
-      <Curso />
+      <h3>React JS Demo</h3> 
+      {
+        cursos.map(el => <Curso title={el.title} profesor={el.profesor} price={el.price} image={el.image}/>)
+      }
     </div>
   </>
 )
--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,25 +1,33 @@
-import React from "react";
+import React from "react"
+import PropTypes from 'prop-types'
 
-const persona = {
-   "nombre":"David",
-   "apellido":"Romero",
-   edad: 35,
-   "curso":"Diseño Web con HTML y CSS"
-}
-
-const Curso = () =>(
+const Curso = ({title,profesor,price,image,alt}) =>(
    <article className="card">
       <div className="card__layout">
          <div className="img-container">
-            <img src="https://placeimg.com/200/100/tech" alt="" />
+            <img src={image} alt={alt} />
          </div>
          <div>
-            <h3>{`Curso: ${persona.curso}`}</h3>
-            <div><span>{`Profesor: ${persona.nombre} ${persona.apellido}`}</span></div>
-            <div><a href="hola.com">Ir al curso</a></div>
+            <h3>{`Curso: ${title}`}</h3>
+            <div><span>{`Profesor: ${profesor}`}</span></div>
+            <div><a href="#">{`$ ${price} USD`}</a></div>
          </div>
       </div>
    </article>
 )
 
+Curso.propTypes = {
+   title: PropTypes.string,
+   image: PropTypes.string,
+   alt: PropTypes.string,
+   profesor: PropTypes.string,
+   price: PropTypes.number
+}
+Curso.defaultProps = {
+   title: 'No hay titulo!!!',
+   image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkxpPuXOcu0Q_6Zl_YOtMenAkcawLb7HtRzg&usqp=CAU',
+   alt: 'No hay Alt!!!',
+   profesor: 'No hay profesor disponible',
+   price: '--'
+}
 export default Curso
